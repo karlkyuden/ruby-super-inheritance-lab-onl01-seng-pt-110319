58190292b8e177ffd5a70bb466390761e9a5fe07
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-14 06:50:42 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,17 +7,6 @@ class ChattyStudent < Student
   
   def raise_hand
     10.times {super}
-    super.time_select
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
   end
   
 end
